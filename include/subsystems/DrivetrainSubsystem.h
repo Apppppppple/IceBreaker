27278e326d4619c8568a35faa9ec44d191e25a9e
Apppppppple <19359968+Apppppppple@users.noreply.github.com>
2021-12-06 18:06:13 -0500

    Finish DrivetrainSubsystem

--- include/subsystems/DrivetrainSubsystem.h
+++ include/subsystems/DrivetrainSubsystem.h
@@ -18,7 +18,12 @@ class DrivetrainSubsystem : public frc2::SubsystemBase {
   using VictorSPX = ctre::phoenix::motorcontrol::can::WPI_VictorSPX;
 
 public:
-  frc::Encoder dtLeftEncoder, dtRightEncoder;
+  frc::Encoder dtLeftEncoder{drive_constants::driveTrainLeftEncoderChannelA,
+                             drive_constants::driveTrainLeftEncoderChannelB,
+                             false, frc::CounterBase::EncodingType::k4X},
+      dtRightEncoder{drive_constants::driveTrainRightEncoderChannelA,
+                     drive_constants::driveTrainRightEncoderChannelB, true,
+                     frc::CounterBase::EncodingType::k4X};
   frc::ADXRS450_Gyro driveGyro;
 
   enum GearShiftStatus { High, Low };
@@ -26,31 +31,51 @@ public:
 
   DrivetrainSubsystem();
 
-  /**
-   * Will be called periodically whenever the CommandScheduler runs.
-   */
-  void Periodic() override;
+  void Drive(double leftSpeed, double rightSpeed);
+  void StopDrive();
 
-  /**
-   * Will be called periodically whenever the CommandScheduler runs during
-   * simulation.
-   */
+  void ResetEncoders();
+
+  double GetLeftEncoder();
+  double GetRightEncoder();
+  double GetAverageEncoder();
+
+  void ResetGyro();
+  double GetGyroAngle();
+  double GetGyroRate();
+
+  void ShiftDown();
+  void ShiftUp();
+  static GearShiftStatus GetGearShiftPosition();
+
+  void Periodic() override;
   void SimulationPeriodic() override;
 
 private:
-  VictorSPX leftMotors[3]{drive_constants::leftMotorChannels[0],
-                          drive_constants::leftMotorChannels[1],
-                          drive_constants::leftMotorChannels[2]},
-      rightMotors[3]{drive_constants::rightMotorChannels[0],
-                     drive_constants::rightMotorChannels[1],
-                     drive_constants::rightMotorChannels[2]};
-  frc::DifferentialDrive m_drive;
+  // Not an array because
+  // https://stackoverflow.com/questions/60606947/initialising-member-aggregate-type-without-copy-constructor
+  // https://stackoverflow.com/questions/29759441/is-a-class-with-deleted-copy-constructor-trivially-copyable
+  // https://stackoverflow.com/questions/38224877/how-to-enforce-copy-elision-why-it-wont-work-with-deleted-copy-constructor
+  VictorSPX leftMotor1{drive_constants::leftMotorChannels[0]},
+      leftMotor2{drive_constants::leftMotorChannels[1]},
+      leftMotor3{drive_constants::leftMotorChannels[2]},
+      rightMotor1{drive_constants::rightMotorChannels[0]},
+      rightMotor2{drive_constants::rightMotorChannels[1]},
+    rightMotor3{drive_constants::rightMotorChannels[2]};
+
+  // TODO: will this get optimized or is it not worth it?
+  VictorSPX *leftMotors[3]{&leftMotor1, &leftMotor2, &leftMotor3},
+      *rightMotors[3]{&rightMotor1, &rightMotor2, &rightMotor3};
+
+  // TODO: Does leftController and rightController need to be stored in the
+  // class? Not really sure about the semantics for references.
+  frc::DifferentialDrive m_drive{leftController, rightController};
 
   frc::DoubleSolenoid gearShiftSolenoid{drive_constants::gearShiftHighChannel,
-    drive_constants::gearShiftLowChannel};
+                                        drive_constants::gearShiftLowChannel};
 
-  frc::SpeedControllerGroup leftDriveMotors{leftMotors[0], leftMotors[1],
-                                            leftMotors[2]};
-  frc::SpeedControllerGroup rightDriveMotors{rightMotors[0], rightMotors[1],
-                                             rightMotors[2]};
+  frc::SpeedControllerGroup leftController{*leftMotors[0], *leftMotors[1],
+                                           *leftMotors[2]};
+  frc::SpeedControllerGroup rightController{*rightMotors[0], *rightMotors[1],
+                                            *rightMotors[2]};
 };
--- src/subsystems/DrivetrainSubsystem.cpp
+++ src/subsystems/DrivetrainSubsystem.cpp
@@ -5,23 +5,100 @@
 #include "subsystems/DrivetrainSubsystem.h"
 
 #include <frc/SpeedControllerGroup.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/CounterBase.h>
+#include <frc/DoubleSolenoid.h>
+#include <frc/drive/DifferentialDrive.h>
 
 #include "Constants.h"
-#include "frc/drive/DifferentialDrive.h"
 
 DrivetrainSubsystem::DrivetrainSubsystem() {
-  leftDriveMotors.SetInverted(true);
-  rightDriveMotors.SetInverted(true);
-  // m_drive = frc::DifferentialDrive(m_left, m)
+  leftController.SetInverted(true);
+  rightController.SetInverted(true);
 
+  m_drive.SetDeadband(0.05);
+  m_drive.SetMaxOutput(drive_constants::driveMaxSpeed());
 
-  // Implementation of subsystem constructor goes here.
+  dtLeftEncoder.SetSamplesToAverage(
+      drive_constants::driveTrainLeftEncoderAverageSamples);
+  dtLeftEncoder.SetMinRate(drive_constants::driveTrainLeftEncoderMinRate);
+  dtLeftEncoder.SetDistancePerPulse(
+      drive_constants::driveTrainLeftEncoderPulseDistance);
+
+  dtRightEncoder.SetSamplesToAverage(
+      drive_constants::driveTrainRightEncoderAverageSamples);
+  dtRightEncoder.SetMinRate(drive_constants::driveTrainRightEncoderMinRate);
+  dtRightEncoder.SetDistancePerPulse(
+      drive_constants::driveTrainRightEncoderPulseDistance);
+
+  driveGyro.Calibrate();
+
+  gearShiftStatus = GearShiftStatus::Low;
+}
+
+void DrivetrainSubsystem::Drive(double leftSpeed, double rightSpeed) {
+  frc::SmartDashboard::PutNumber("Left Drive Speed: ", leftSpeed);
+  frc::SmartDashboard::PutNumber("Right Drive Speed: ", rightSpeed);
+  leftSpeed = leftSpeed * drive_constants::driveSpeedMultiplier();
+  rightSpeed = rightSpeed * drive_constants::driveSpeedMultiplier();
+  m_drive.TankDrive(leftSpeed, rightSpeed, false);
+}
+
+void DrivetrainSubsystem::StopDrive() {
+  m_drive.TankDrive(0, 0, false);
+}
+
+void DrivetrainSubsystem::ResetEncoders() {
+  dtLeftEncoder.Reset();
+  dtRightEncoder.Reset();
+}
+
+double DrivetrainSubsystem::GetLeftEncoder() {
+  return dtLeftEncoder.GetDistance();
+}
+
+double DrivetrainSubsystem::GetRightEncoder() {
+  return dtRightEncoder.GetDistance();
+}
+
+double DrivetrainSubsystem::GetAverageEncoder() {
+  return (GetLeftEncoder() + GetRightEncoder()) / 2;
+}
+
+void DrivetrainSubsystem::ResetGyro() {
+  driveGyro.Reset();
+}
+
+double DrivetrainSubsystem::GetGyroAngle() {
+  return driveGyro.GetAngle();
+}
+
+double DrivetrainSubsystem::GetGyroRate() {
+  return driveGyro.GetRate();
+}
+
+void DrivetrainSubsystem::ShiftDown() {
+  gearShiftSolenoid.Set(frc::DoubleSolenoid::Value::kForward); // kForward is Low (down) speed
+  gearShiftStatus = GearShiftStatus::Low;
+}
+
+void DrivetrainSubsystem::ShiftUp() {
+  gearShiftSolenoid.Set(frc::DoubleSolenoid::Value::kReverse); // KReverse is High (up) speed
+  gearShiftStatus = GearShiftStatus::High;
+}
+
+DrivetrainSubsystem::GearShiftStatus DrivetrainSubsystem::GetGearShiftPosition() {
+  return gearShiftStatus;
 }
 
 void DrivetrainSubsystem::Periodic() {
-  // Implementation of subsystem periodic method goes here.
+  frc::SmartDashboard::PutNumber("Gyro Angle: ", GetGyroAngle());
+  frc::SmartDashboard::PutNumber("Left Drive Encoder: ", GetLeftEncoder());
+  frc::SmartDashboard::PutNumber("Right Drive Encoder: ", GetRightEncoder());
 }
 
 void DrivetrainSubsystem::SimulationPeriodic() {
-  // Implementation of subsystem simulation periodic method goes here.
+  Periodic();
 }
+
+DrivetrainSubsystem::GearShiftStatus DrivetrainSubsystem::gearShiftStatus = GearShiftStatus::Low;
